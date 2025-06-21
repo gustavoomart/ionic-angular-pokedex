a9cea5adc03144614c4fdc1f4b59b22f5bf4544c
gustavoomartinn <gustavomartinsv@gmail.com>
2025-06-21 03:44:11 -0300

    feat(pages): páginas iniciais de details e favorites criadas

---
+++ src/app/pages/datails/details.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'details',
+    templateUrl: 'details.page.html',
+    styleUrls: [
+        'details.page.scss',
+    ],
+    standalone: true,
+    imports: [
+        
+    ],
+    providers: []
+})
+export class DetailsPage implements OnInit {
+    constructor() {}
+
+    ngOnInit() {
+
+    }
+}
---
+++ src/app/pages/favorites/favorites.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'favorites',
+    templateUrl: 'favorites.page.html',
+    styleUrls: [
+        'favorites.page.scss'
+    ],
+    standalone: true,
+    imports: [
+
+    ],
+    providers: []
+})
+export class FavoritesPage implements OnInit {
+    constructor() { }
+
+    async ngOnInit() {
+
+    }
+}
